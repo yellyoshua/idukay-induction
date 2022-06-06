caf50ad1ed4757fbe6a6849010ef912881ed44d4
yellyoshua <usuario1529yoshua@hotmail.com>
2022-06-06 11:22:47 -0500

    implementing user code generator as default

--- api/src/models/users.model.js
+++ api/src/models/users.model.js
@@ -1,6 +1,13 @@
 import mongoose from "mongoose";
+import { generateUserCode } from "../controllers/userCode.controller.js";
 
 const usersSchema = new mongoose.Schema({
+    code: {
+        type: String,
+        default: generateUserCode,
+        unique: true,
+        required: true,
+    },
     name: {
         type: String,
         required: true,
