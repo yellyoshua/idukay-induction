02649afde09d5bbbefed3513e9e3255633909208
yellyoshua <usuario1529yoshua@hotmail.com>
2022-06-06 11:26:30 -0500

    add icons and styles

---
+++ induction-user-form/src/icons/CheckIcon.jsx
@@ -0,0 +1,5 @@
+export default function CheckIcon({ width, height, color, ...props }) {	
+	return <svg xmlns="http://www.w3.org/2000/svg" {...{width, height, color, ...props}} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+		<path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+	</svg>
+}
