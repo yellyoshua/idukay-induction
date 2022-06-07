9e64b498809c9656e4e7500e1f86e9f41b707296
yellyoshua <me@yoshualopez.com>
2022-06-07 17:25:50 -0500

    refactor

--- hackerrank/adjacent_words.js
+++ hackerrank/adjacent_words.js
@@ -1,21 +1,19 @@
 function adjacentWords(words = []) {
-    let acc = 0;
+    let replaces = 0;
     
     while(words.length) {
         const current = words.shift();
         const next = words[0];
 
         if (current === next) {
-            let nTimes = 1;
+            let times = 1;
             while (words[0] === current)
-                nTimes++, words.shift();
-
-            const replaces = Math.floor((nTimes / 2));
-            acc += replaces;
+                times++, words.shift();
+            replaces += Math.floor(times / 2);
         }
     }
 
-    return acc
+    return replaces
 }
 
 module.exports = (words = []) => {
